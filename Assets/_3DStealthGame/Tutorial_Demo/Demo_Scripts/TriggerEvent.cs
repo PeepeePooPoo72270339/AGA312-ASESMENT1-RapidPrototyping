cfc07f7d5e5428f251cb19e9b8f0aa760cd2ec08
PeepeePooPoo72270339 <onezeldafan@gmail.com>
2026-02-17 18:48:42 +1100

    Updated triggerEvent
    Trigger event for colliding with stuff has been updated to be more dynamic

--- Assets/_3DStealthGame/Tutorial_Demo/Demo_Scripts/TriggerEvent.cs
+++ Assets/_3DStealthGame/Tutorial_Demo/Demo_Scripts/TriggerEvent.cs
@@ -3,15 +3,34 @@ using UnityEngine.Events;
 
 public class TriggerEvent : MonoBehaviour
 {
-
+    public string[] checkTag;
     public UnityEvent OnTriggerEnterEvent;
     public UnityEvent OnTriggerExitEvent;
     public UnityEvent OnTriggerStayEvent;
 
     public void OnTriggerEnter(Collider other)
     {
-        OnTriggerEnterEvent?.Invoke();
-        OnTriggerExitEvent?.Invoke();
+        for (int i = 0; i < checkTag.Length; i++)
+        {
+            if (other.gameObject.CompareTag(checkTag[i]))
+            OnTriggerEnterEvent?.Invoke();
+
+        }
+     
+    
     }
+
+    public void OnTriggerExit(Collider other)
+    {
+        for (int i = 0; i < checkTag.Length; i++)
+        {
+            if (other.gameObject.CompareTag(checkTag[i]))
+                OnTriggerExitEvent?.Invoke();
+
+        }
+
+
+    }
+
 
 }
