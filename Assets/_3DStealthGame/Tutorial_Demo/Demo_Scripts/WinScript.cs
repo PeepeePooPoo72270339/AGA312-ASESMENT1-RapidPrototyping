3b42d85f894c75192ac424bf31ecf1fc7eb78ccc
PeepeePooPoo72270339 <onezeldafan@gmail.com>
2026-03-06 14:52:01 +1100

    Added wincon
    Added a script to check if all monsters are dead a quick way to check said list

--- Assets/_3DStealthGame/Tutorial_Demo/Demo_Scripts/WinScript.cs
+++ Assets/_3DStealthGame/Tutorial_Demo/Demo_Scripts/WinScript.cs
@@ -1,26 +1,47 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
+using System;
 
 public class WinScript : MonoBehaviour
 {
-    public GameObject[] Monsters;
+    public List<GameObject> Monsters = new List<GameObject>();
     public int GhostsRemaining;
     private VisualElement m_EndScreen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        GhostsRemaining = Monsters.Length;
-        if (Monsters.Length == 0)
-        { 
-        
-        
+        if (Monsters.Count > 0)
+        {
+            GhostsRemaining = Monsters.Count;
+
+        }
+        if (Monsters.Count == 0)
+        {
+            print("YouWon");
+            GhostsRemaining = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+                Monsters.RemoveAt(0);
+                Monsters.RemoveAt(1);
+                Monsters.RemoveAt(2);
+                Monsters.RemoveAt(3);
+
+
+
         }
         
+        
     }
+    
+
+
+
 }
