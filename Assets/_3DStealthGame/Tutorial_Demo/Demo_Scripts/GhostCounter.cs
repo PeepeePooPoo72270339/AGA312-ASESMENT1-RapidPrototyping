f30bffbfacb67d3acac0a43ff8b9065ed0cf3a3f
PeepeePooPoo72270339 <onezeldafan@gmail.com>
2026-03-09 14:40:05 +1100

    UI now rendering
    Game now properly renders the ghost counter (but does count the ghosts yet)

---
+++ Assets/_3DStealthGame/Tutorial_Demo/Demo_Scripts/GhostCounter.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GhostCounter : MonoBehaviour
+{
+    public GameObject GameManager;
+    public TMP_Text GhostRemaining;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GhostRemaining.text = "test";
+    }
+}
