1bdf658e8e7a52bd193b3a1372311e8da6d944fa
PeepeePooPoo72270339 <onezeldafan@gmail.com>
2026-03-09 15:05:16 +1100

    UI
    Added all UI elements and they all function

--- Assets/_3DStealthGame/Tutorial_Demo/Demo_Scripts/GhostCounter.cs
+++ Assets/_3DStealthGame/Tutorial_Demo/Demo_Scripts/GhostCounter.cs
@@ -15,6 +15,7 @@ public class GhostCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GhostRemaining.text = "test";
+        GhostRemaining.text = GameManager.GetComponent<WinScript>().GhostsRemaining.ToString();
+
     }
 }
